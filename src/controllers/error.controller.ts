d9d81f387117e791eca95c0b26fb33c091d10ad9
Ananda Bintang <anandabintang4@gmail.com>
2024-03-07 22:51:08 +0700

    add jwt parse and jwt verify

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,4 +1,5 @@
 import { type NextFunction, type Request, type Response } from 'express'
+import { verifyAccessToken } from '../utils/jwt'
 import logger from '../utils/winston'
 
 export const errorHandling = (
@@ -26,4 +27,32 @@ export const notFound = (
     message: 'Not found!',
     data: null
   })
+}
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  const authHeader = req.headers.authorization
+  const token = authHeader?.split(' ')[1]
+
+  if (token === undefined) {
+    return res.status(401).json({
+      error: 'Unauthorized!',
+      message: 'You need to login first!',
+      data: null
+    })
+  }
+
+  const user = verifyAccessToken(String(token))
+  if (user === null) {
+    return res.status(403).json({
+      error: 'Forbidden!',
+      message: 'Invalid token!',
+      data: null
+    })
+  }
+
+  next()
 }
--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { getAllItem, inputItem } from '../controllers/item.controller'
+import { authenticate } from '../controllers/error.controller'
 
 const itemRouter = Router()
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-itemRouter.get('/item', getAllItem)
-itemRouter.post('/item', inputItem)
+itemRouter.get('/item', authenticate, getAllItem)
+itemRouter.post('/item', authenticate, inputItem)
 
 export default itemRouter
